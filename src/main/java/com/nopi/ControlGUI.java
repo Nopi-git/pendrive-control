34f86c342f4258898597f2bfb31e988227f9c31e
Nopi96912 <nopiwork@gmail.com>
2018-04-30 20:21:10 +0200

    basic menu

--- src/main/java/com/nopi/ControlGUI.java
+++ src/main/java/com/nopi/ControlGUI.java
@@ -1,5 +1,31 @@
 package com.nopi;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
 public class ControlGUI {
+
+    public static HBox addMenuButtons(){
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(25, 12, 15, 12));
+        hBox.setSpacing(10);
+
+        //Control
+        Button controlButton = new Button("Control");
+        controlButton.setPrefWidth(100);
+
+        //Cash
+        Button cashButton = new Button("Cash");
+        cashButton.setPrefWidth(100);
+
+        //Error
+        Button errorButton = new Button("Error");
+        errorButton.setPrefWidth(100);
+
+        hBox.getChildren().addAll(controlButton, cashButton, errorButton);
+
+        return hBox;
+    }
 
 }
--- src/main/java/com/nopi/PendriveController.java
+++ src/main/java/com/nopi/PendriveController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -22,14 +23,16 @@ import java.io.IOException;
 
 public class PendriveController extends Application {
 
+    Scene menu, controlPage, cashPage, errorPage;
+
     public static void main(String[] args) {
 
-        try{
+        try {
             PendriveUtility pendriveUtility = new PendriveUtility();
-            if(pendriveUtility.serialInList("6B01A8414EC9Da") || true){
+            if (pendriveUtility.serialInList("6B01A8414EC9Da") || true) {
                 launch(args);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getStackTrace());
         }
     }
@@ -38,25 +41,13 @@ public class PendriveController extends Application {
     public void start(Stage primaryStage) throws IOException {
 
 
-        Control control = new Control("dev");
+        //Control control = new Control("dev");
 
         primaryStage.setTitle("ArminBet-Control");
 
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        gridPane.setPadding(new Insets(25,25,25,25));
+        menu = new Scene(ControlGUI.addMenuButtons(), 320, 80);
 
-        Text scenetitle = new Text("Control");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        gridPane.add(scenetitle, 0, 0, 2, 1);
-        
-
-        Scene scene = new Scene(gridPane, 500,275);
-
-        primaryStage.setScene(scene);
-
+        primaryStage.setScene(menu);
         primaryStage.show();
     }
 }
